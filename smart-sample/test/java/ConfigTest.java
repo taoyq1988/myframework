cd5ff346838550ed77da6123898c37d4f453317a
tyq <892622580@qq.com>
2017-11-28 12:34:35 +0800

    web项目

---
+++ smart-sample/test/java/ConfigTest.java
@@ -0,0 +1,16 @@
+package java;
+
+import org.junit.Test;
+
+/**
+ * @author tyq
+ * @version 1.0, 2017/11/28
+ */
+public class ConfigTest {
+
+    @Test
+    public void readTest() {
+        String s = "d";
+        System.out.println(s);
+    }
+}
