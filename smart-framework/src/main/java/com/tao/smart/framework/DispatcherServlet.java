c6de3070c5c3b8dbd5890b710c8af012e61c5858
tyq <892622580@qq.com>
2017-11-28 09:59:40 +0800

    拦截器完成

--- smart-framework/src/main/java/com/tao/smart/framework/DispatcherServlet.java
+++ smart-framework/src/main/java/com/tao/smart/framework/DispatcherServlet.java
@@ -1,9 +1,18 @@
 package com.tao.smart.framework;
 
+import com.tao.smart.framework.bean.Data;
 import com.tao.smart.framework.bean.Handler;
+import com.tao.smart.framework.bean.Param;
+import com.tao.smart.framework.bean.View;
 import com.tao.smart.framework.helper.BeanHelper;
 import com.tao.smart.framework.helper.ConfigHelper;
 import com.tao.smart.framework.helper.ControllerHelper;
+import com.tao.smart.framework.utils.CodeUtils;
+import com.tao.smart.framework.utils.JsonUtils;
+import com.tao.smart.framework.utils.ReflectionUtils;
+import com.tao.smart.framework.utils.StreamUtils;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
@@ -14,6 +23,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Method;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 请求转发器
@@ -40,17 +54,71 @@ public class DispatcherServlet extends HttpServlet {
 
     @Override
     public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //
+        //获取请求方法与路径
         String requestMethod = request.getMethod().toLowerCase();
         String requestPath = request.getPathInfo();
-        //
+        //获取Action处理器
         Handler handler = ControllerHelper.getHandler(requestMethod, requestPath);
         if (handler != null) {
-            //
+            //获取Controller类和其bean实例
             Class<?> controllerClass = handler.getControllerClass();
             Object controllerBean = BeanHelper.getBean(controllerClass);
-            //
-            //todo
+            //创建请求参数对象
+            Map<String, Object> paramMap = new HashMap<>();
+            Enumeration<String> paramNames = request.getAttributeNames();
+            while (paramNames.hasMoreElements()) {
+                String paramName = paramNames.nextElement();
+                String paramValue = request.getParameter(paramName);
+                paramMap.put(paramName, paramValue);
+            }
+            String body = CodeUtils.encodeURL(StreamUtils.getString(request.getInputStream()));
+            if (StringUtils.isNotEmpty(body)) {
+                String[] params = StringUtils.split(body, "&");
+                if (ArrayUtils.isNotEmpty(params)) {
+                    for (String param : params) {
+                        String[] array = StringUtils.split(param, "=");
+                        if (ArrayUtils.isNotEmpty(array) && array.length == 2) {
+                            String paramName = array[0];
+                            String paramValue = array[1];
+                            paramMap.put(paramName, paramValue);
+                        }
+                    }
+                }
+            }
+            //调用action方法
+            Param param = new Param(paramMap);
+            Method actionMethod = handler.getActionMethod();
+            Object result = ReflectionUtils.invokeMethod(controllerBean, actionMethod, param);
+            //处理action方法返回值
+            if (result instanceof View) {
+                //返回jsp对象
+                View view = (View) result;
+                String path = view.getPath();
+                if (StringUtils.isNotEmpty(path)) {
+                    if (path.startsWith("/")) {
+                        response.sendRedirect(request.getContextPath() + path);
+                    } else {
+                        Map<String, Object> model = view.getModel();
+                        for (Map.Entry<String, Object> entry : model.entrySet()) {
+                            request.setAttribute(entry.getKey(), entry.getValue());
+                        }
+                        request.getRequestDispatcher(ConfigHelper.getAppJspPath() + path).forward(request, response);
+                    }
+                }
+            } else if (request instanceof Data) {
+                //返回json数据
+                Data data = (Data) request;
+                Object model = data.getModel();
+                if (model != null) {
+                    response.setContentType("application/json");
+                    response.setCharacterEncoding("UTF-8");
+                    PrintWriter writer = response.getWriter();
+                    String json = JsonUtils.toJson(model);
+                    writer.write(json);
+                    writer.flush();
+                    writer.close();
+                }
+            }
         }
     }
 }
