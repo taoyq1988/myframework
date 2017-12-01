471c417dcc44f8f2acef302cf5c2b66355778214
tyq <892622580@qq.com>
2017-12-01 10:38:38 +0800

    日志测试

--- smart-sample/src/main/java/com/tao/smart/sample/service/impl/TestServiceImpl.java
+++ smart-sample/src/main/java/com/tao/smart/sample/service/impl/TestServiceImpl.java
@@ -3,6 +3,8 @@ package com.tao.smart.sample.service.impl;
 import com.tao.smart.framework.annotation.Service;
 import com.tao.smart.framework.annotation.Transaction;
 import com.tao.smart.sample.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author tyq
@@ -11,9 +13,13 @@ import com.tao.smart.sample.service.TestService;
 @Service
 public class TestServiceImpl implements TestService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestServiceImpl.class);
+
     @Override
     @Transaction
     public void hello(String name) {
+        LOGGER.info("log info test");
+        LOGGER.debug("log debug test");
         System.out.println("hello " + name);
     }
 }
